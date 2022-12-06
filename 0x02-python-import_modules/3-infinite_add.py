d9fc5187bbf456baaa21b00327747299ef9625b3
elvis-ux <chukwukereelvis@gmail.com>
2022-12-05 19:35:42 -0800

    3-infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import sys
-total = 0
-
-for i in range(len(sys.argv) - 1):
-    total += int(sys.argv[i + 1])
-
-    print("{}".format(total))
+    from sys import argv
+    varLen = len(argv) - 1
+    result = 0
+    for i in range(1, varLen + 1):
+        result += int(argv[i])
+        print(result)
