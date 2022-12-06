07f074de45b34605ee030d6df93e17be3c7fc52f
elvis-ux <chukwukereelvis@gmail.com>
2022-12-05 19:50:01 -0800

    done

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__name__":
     import hidden_4
-
-    for item in dir(hidden_4):
-        if item.startswith('__'):
-            continue
-        print(item)
+name = dir(hidden_4)
+for names in name:
+    if names[:2] != "__":
+        print(names)
